af55817d09c2f72fc5d2e46f8100fc848ea42bdf
tilzz25 <tilakp1151@gmail.com>
2025-06-16 16:52:42 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Components
 import Navbar from './components/navbar';
-import Footer from './components/footer';
 import Home from './components/home';
-import Products from './components/productList';
+import ProductList from './components/productList';
 import Cart from './components/cart';
 import Checkout from './components/checkout';
+import Footer from './components/footer';
+import { CartProvider } from './components/cart';
+import './styles/App.css';
 
-
-
-
-// Context
-import { CartProvider } from './context/context';
 
 function App() {
   return (
     <CartProvider>
       <Router>
-        <div className="flex flex-col min-h-screen">
+        <div className="app-container">
           <Navbar />
-
-          <main className="flex-grow">
+          <main className="main-content">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/products" element={<Products />} />
+              <Route path="/products" element={<ProductList />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/checkout" element={<Checkout />} />
             </Routes>
           </main>
-
           <Footer />
         </div>
       </Router>
